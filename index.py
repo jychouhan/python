5630c105155ed19ff8549e708486528e9f7e38a7
jychouhan <jyoti.chouhan9522@gmail.com>
2024-01-09 18:47:02 +0530

    Added

---
+++ index.py
@@ -0,0 +1,12 @@
+print("this is my first python code")
+print("my name is jyoti")
+a = 30
+b = 60
+print(a+b)
+c = "jyoti"
+print(c)
+print(type(c))
+## variable should be a single word and start with alphabet only
+## we can join words of variable using underscore(_) sign only
+
+
