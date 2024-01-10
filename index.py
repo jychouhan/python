848355e9ff5ce764ed92a3655059ade37f76dd0f
jychouhan <jyoti.chouhan9522@gmail.com>
2024-01-10 18:43:00 +0530

    work with conditional statement in python

--- index.py
+++ index.py
@@ -8,5 +8,38 @@ print(c)
 print(type(c))
 ## variable should be a single word and start with alphabet only
 ## we can join words of variable using underscore(_) sign only
+print(5*2)
+print(18%5)
+print(20/3)
+print(6**3)
+print(17//4)
+
+a = 10
+b = 36
+if a>b:
+    print("a is greater")
+else:
+    print("b is greater")
+print(10>6)
+print(10>=8)
+print(10==6)
+print(16!=4)
+g = 5
+g += 10
+print(g)
+
+a = input("enter the value of a :") ##input function always give in  
+b = input("enter the value of b :")
+c = input("enter the value of c :")
+print(type (a))
+
+a = int(input("enter the value of a :"))
+b = int(input("enter the value of b :"))
+if a>b:
+    print("a is greater than b")56
+else:
+    print("a is less than b")
+
+
 
 
