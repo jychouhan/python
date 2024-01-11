bfc2a6d9da1bd13f8c1b34616d882a28f7863727
jychouhan <jyoti.chouhan9522@gmail.com>
2024-01-11 18:36:28 +0530

    work with if else

--- index.py
+++ index.py
@@ -1,45 +1,101 @@
-print("this is my first python code")
-print("my name is jyoti")
-a = 30
-b = 60
-print(a+b)
-c = "jyoti"
-print(c)
-print(type(c))
-## variable should be a single word and start with alphabet only
+#print("this is my first python code")
+#print("my name is jyoti")
+#a = 30
+#b = 60
+#print(a+b)
+#c = "jyoti"
+#print(c)
+#print(type(c))
+## variable should be a single word and starts with alphabet only
 ## we can join words of variable using underscore(_) sign only
-print(5*2)
-print(18%5)
-print(20/3)
-print(6**3)
-print(17//4)
+#print(5*2)
+#print(18%5)
+#print(20/3)
+#print(6**3)
+#print(17//4)
 
-a = 10
-b = 36
-if a>b:
-    print("a is greater")
-else:
-    print("b is greater")
-print(10>6)
-print(10>=8)
-print(10==6)
-print(16!=4)
-g = 5
-g += 10
-print(g)
+#a = 10
+#b = 36
+#if a>b:
+#   print("a is greater")
+#else:
+#    print("b is greater")
+#print(10>6)
+#print(10>=8)
+#print(10==6)
+#print(16!=4)
+#g = 5
+#g += 10
+#print(g)
 
-a = input("enter the value of a :") ##input function always give in  
-b = input("enter the value of b :")
-c = input("enter the value of c :")
-print(type (a))
-
-a = int(input("enter the value of a :"))
-b = int(input("enter the value of b :"))
-if a>b:
-    print("a is greater than b")56
-else:
-    print("a is less than b")
+#a = int(input("enter the first number"))
+#if (a%2)==0:
+#    print("a is even")
+#else:
+#    print("a is odd")
 
 
+#a = input("enter the value of a :") ##input function always give in  
+#b = input("enter the value of b :")
+#c = input("enter the value of c :")
+#print(type (a))
+
+#a = int(input("enter the value of a :"))
+#b = int(input("enter the value of b :"))
+#if a>b:
+#    print("a is greater than b")
+#else:
+#    print("a is less than b")
+
+#a =int(input("enter a number"))
+#if a<0:
+#    print("a is negative")
+#else:
+#    print("a is negative")
+
+#a = int(input("enter the value of a"))
+#b = int(input("enter the value of b"))
+#c = int(input("enter the value of c"))
+#if a>b and a>c:
+#    print("a is greater than b and c")
+#elif b>a and b>c:
+#    print("b is greater than a and c")
+#else:
+#    print("c is greater than a and b")
+
+
+#a = int(input("enter the value of day  :"))
+#if a == 0:
+#    print("Today is Sunday")
+#elif a == 1:
+#    print("Today is monday")
+#elif a == 2:
+#    print("Today is Tuesday")
+#elif a == 3:
+#    print("Today is wednesday")
+#elif a == 4:
+#    print("Today is Thursday")
+#elif a ==5:
+#    print("Today is Friday")
+#elif a ==6:
+#    print("Today is Saturday")
+#else :
+#    print("invalid number")
+
+#a = input("give input  :")
+#if a>="a" and a<="z":
+#    print("alphabet")
+#elif a>="0" and a<="9":
+#    print("numeric")
+#else:
+#    print("special character")
+
+#A = input("give value  :")
+#if A == "a,e,i,o,u":
+#    print("this is Vowel")
+#else:
+#    print("This is Consonent")
+
+     
 
 
